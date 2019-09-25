dc0baa4c40d5dbb195f14c60bb4c735f430e9706
archie <aztek.patna@gmail.com>
2019-09-25 16:15:32 +0530

    first spring push

---
+++ src/main/java/com/archie/tacocloud/TacoCloudApplication.java
@@ -0,0 +1,15 @@
+package com.archie.tacocloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoCloudApplication
+{
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(TacoCloudApplication.class, args);
+	}
+
+}
